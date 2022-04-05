eeb0b1d8b774779c54961f339787308ae90136d8
Robert-0413 <97753799+Robert-0413@users.noreply.github.com>
2022-04-05 13:18:21 -0400

    Add files via upload
    CRUD Product Page

---
+++ demo/src/main/java/GP3Project/project/Gp3ProjectApplication.java
@@ -0,0 +1,13 @@
+package GP3Project.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gp3ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gp3ProjectApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/GP3Project/project/Gp3ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package GP3Project.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gp3ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
